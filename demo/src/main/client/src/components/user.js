067c82d75da8184a594d4e60767dd01888f1a29b
sarth830 <sarthjain830@gmail.com>
2024-04-18 22:15:51 -0400

    added latest first build for production

--- demo/src/main/client/src/components/user.js
+++
@@ -1,5 +0,0 @@
-export const mainUser = {
-    id: 'a',
-    username: 'Swastik Patro',
-    img: 'https://res.cloudinary.com/dtbd1y4en/image/upload/v1688980483/Gutargu-social/psafyt10mpcsh64esgso.jpg',
-};
